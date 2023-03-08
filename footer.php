82f818bc4b49b27326b2830ff14779de2a091292
denizbinay <deniz@bombig.net>
2023-03-08 16:23:45 +0100

    add footer links

--- footer.php
+++ footer.php
@@ -1,13 +1,14 @@
 <footer class="footer-container">
     <ul class="footer-menu-list">
-        <li>Impressum</li>
-        <li>Datenschutz</li>
-        <li>AGB</li>
+        <li><a href="https://www.zipse.de/servicemenu/impressum.html">Impressum</a></li>
+        <li><a href="https://www.zipse.de/servicemenu/datenschutz.html">Datenschutz</a></li>
+        <li><a href="https://www.zipse.de/unternehmen/agb.html">AGB</a></li>
     </ul>
     <ul class="footer-social-list">
-        <li> <img class="footer-social-icon" src="img/social-facebook.png" alt="social facebook"> </li>
-        <li> <img class="footer-social-icon" src="img/social-linkedin.png" alt="social linkedin"> </li>
-        <li> <img class="footer-social-icon" src="img/social-instagram.png" alt="social instagram"> </li>
-        <li> <img class="footer-social-icon" src="img/social-youtube.png" alt="social youtube"> </li>
+        <li> <a href="https://www.facebook.com/ZipseFachhandel"><img class="footer-social-icon" src="img/social-facebook.png" alt="social facebook"> </a> </li>
+        <li> <a href="https://www.linkedin.com/company/zipse-gmbh-co-kg/
+"><img class="footer-social-icon" src="img/social-linkedin.png" alt="social linkedin"> </a> </li>
+        <li> <a href="https://www.instagram.com/zipse_/"><img class="footer-social-icon" src="img/social-instagram.png" alt="social instagram"> </a> </li>
+        <li> <a href=" https://www.youtube.com/channel/UC2ieFixtqGxj0-_WE9G6vFg"><img class="footer-social-icon" src="img/social-youtube.png" alt="social youtube"> </a> </li>
     </ul>
 </footer>
