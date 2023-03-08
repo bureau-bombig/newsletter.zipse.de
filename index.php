42928baa4cbdf1177182bbad86850551850cbcdf
denizbinay <deniz@bombig.net>
2023-03-08 13:50:59 +0100

    exclude header and footer

--- index.php
+++ index.php
@@ -1,3 +1,73 @@
+<?php
+/* _______________________________________________________________________________________________________________________________________________
+	 *
+	 * Contact creation via API
+	 * © copyright 2023 XQueue GmbH
+	 *
+	 * You need the PHP API Client to run this script
+	 * https://dev.maileon.com/api/maileon-api-clients/
+	 * _______________________________________________________________________________________________________________________________________________
+	 */
+
+// Update with your version of the PHP API Client
+require_once('./client/MaileonApiClient.php');
+
+$config = array(
+  'BASE_URI' => 'https://api.maileon.com/1.0',
+  'API_KEY' => '8tgpuugy-IKVgMzGK8yGTHrWyAYLgInKDZS1',
+  'THROW_EXCEPTION' => true,
+  'TIMEOUT' => 60,
+  'DEBUG' => false // NEVER enable on production
+);
+
+if (isset($_POST['email'])) {
+  $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+
+  $standard_0 = $_POST["standard_0"];
+  $standard_1 = $_POST["standard_1"];
+  $standard_2 = $_POST["standard_2"];
+  $standard_3 = $_POST["standard_3"];
+  $standard_4 = $_POST["standard_4"];
+
+  $contactsService = new com_maileon_api_contacts_ContactsService($config);
+
+  $getContact = $contactsService->getContactByEmail($email);
+  if ($getContact->isSuccess() && $getContact->getResult()->permission != com_maileon_api_contacts_Permission::$NONE) {
+    $warning['message'] = 'Contact already exists';
+  } else {
+    $newContact = new com_maileon_api_contacts_Contact();
+    $newContact->email = $email;
+    $newContact->anonymous = false;
+    $newContact->permission = com_maileon_api_contacts_Permission::$NONE;
+
+    $newContact->standard_fields["SALUTATION"] = $standard_0;
+    $newContact->standard_fields["FIRSTNAME"] = $standard_1;
+    $newContact->standard_fields["LASTNAME"] = $standard_2;
+    $newContact->standard_fields["ZIP"] = $standard_3;
+    $newContact->standard_fields["CITY"] = $standard_4;
+    if (isset($_POST['doiSrc'])) {
+      $doiSrc = filter_input(INPUT_POST, 'doiSrc', FILTER_SANITIZE_STRING);
+    } else {
+      $doiSrc = '';
+    }
+    if (isset($_POST['doiSubscriptionPage'])) {
+      $doiSubscriptionPage = filter_input(INPUT_POST, 'doiSubscriptionPage', FILTER_SANITIZE_STRING);
+    } else {
+      $doiSubscriptionPage = '';
+    }
+    $response = $contactsService->createContact($newContact, com_maileon_api_contacts_SynchronizationMode::$UPDATE, $doiSrc, $doiSubscriptionPage, true, true, 'ts1GM1iz');
+  }
+} else {
+  $email = "";
+
+  $standard_0 = "";
+  $standard_1 = "";
+  $standard_2 = "";
+  $standard_3 = "";
+  $standard_4 = "";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +75,7 @@
   <meta charset="UTF-8" />
   <link rel="icon" type="image/svg+xml" href="./assets/avatar.png" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Zipse</title>
+  <title>Zipse Newsletteranmeldung</title>
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
@@ -13,128 +83,167 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <style>
+    .form-signin {
+      width: 100%;
+      max-width: 850px;
+      padding: 15px;
+      margin: 0 auto;
+    }
+  </style>
   <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
-  <div class="container">
-    <header class="header">
-      <div class="logo-container">
-        <!-- Logo Here -->
-        <img class="logo" src="img/logo.png" alt="Logo">
-      </div>
-      <nav class="nav">
-        <!-- Nav Here -->
-        <ul class="nav-list">
-          <li class="nav-item">
-            <a title="Böden" href="https://www.zipse.de/boden.html">Böden</a>
+  <div class="site-container">
+    <?php include('./header.php'); ?>
+    <main>
+      <section>
+        <img class="hero-image" src="img/hero.jpg" alt="Hero">
+      </section>
+
+      <section class="feature-container">
+        <ul class="feature-list">
+          <li class="feature-item"><img class="feature-image" src="img/benefits-1.png" alt="benefits-1">
+            <p class="feature-text">Immer
+              up-to-date</p>
           </li>
-          <li class="nav-item">
-            <a title="Türen" href="https://www.zipse.de/tueren.html">Türen</a>
+          <li class="feature-item"><img class="feature-image" src="img/benefits-2.png" alt="benefits-2">
+            <p class="feature-text">Spezielle Aktionen
+              & Veranstaltungen</p>
           </li>
-          <li class="nav-item">
-            <a title="Garten und Grillen" href="https://www.zipse.de/garten-grillen.html">Garten & Grillen</a>
-          </li>
-          <li class="nav-item">
-            <a title="Stellenangebote" href="https://www.zipse.de/unternehmen/stellenangebote.html">Stellenangebote</a>
+          <li class="feature-item"><img class="feature-image" src="img/benefits-3.png" alt="benefits-3">
+            <p class="feature-text">Prospekte und
+              Produktnews</p>
           </li>
         </ul>
-      </nav>
-    </header>
-    <!-- <main>
-    <section>
-      <img class="hero-image" src="img/hero.jpg" alt="Hero">
-    </section>
-    <section>
-      <ul class="flex flex-row py-12 bg-gray-100">
-        <li class="flex flex-col items-center w-1/3"><img class="w-12 md:w-16 mb-4" src="img/benefits-1.png" alt="benefits-1">
-          <p class="max-w-48 text-center">Immer
-            up-to-date</p>
-        </li>
-        <li class="flex flex-col items-center w-1/3"><img class="w-12 md:w-16 mb-4" src="img/benefits-2.png" alt="benefits-2">
-          <p class="max-w-48 text-center">Spezielle Aktionen
-            & Veranstaltungen</p>
-        </li>
-        <li class="flex flex-col items-center w-1/3"><img class="w-12 md:w-16 mb-4" src="img/benefits-3.png" alt="benefits-3">
-          <p class="max-w-48 text-center">Prospekte und
-            Produktnews</p>
-        </li>
-      </ul>
-    </section>
-    <section class="p-8">
-      <div>
-        <h1 class="text-5xl font-bold mb-4">Anmeldung zum ZIPSE-Newsletter</h1>
-        <p class="mb-8">Verpassen Sie nie wieder neue Produkte, Aktionen, Veranstaltungen und vieles mehr.
-          Wir halten Sie auf dem Laufenden! Das ganze bequem per E-Mail.</p>
-      </div>
-      <div class="flex flex-row">
+      </section>
+
+      <section class="form-container">
         <div>
+          <h1 class="form-headline">Anmeldung zum ZIPSE-Newsletter</h1>
+          <p class="form-text">Verpassen Sie nie wieder neue Produkte, Aktionen, Veranstaltungen und vieles mehr. <br>
+            Wir halten Sie auf dem Laufenden! Das ganze bequem per E-Mail.</p>
         </div>
-        <div>
-          <img src="img/voucher.png" alt="voucher">
+        <div class="bb-form-row">
+          <div class="bb-form-item">
+            <form id="api-data-form" class="form-signin" action="<?= $_SERVER['PHP_SELF'] ?>" method="post" accept-charset="utf-8" role="form">
+              <div class="form-group row">
+                <label class="col-form-label col-sm-3" for="email">Email: <span title="mandatory field">*</span></label>
+                <div class="col-sm-9">
+                  <input type="email" name="email" id="email" maxlength="255" placeholder="- Please enter email address -" value="<?= $email ?>" class="form-control" required>
+                </div>
+              </div>
+
+              <div class="form-group row">
+                <label class="col-form-label col-sm-3" for="standard_0">Anrede:</label>
+                <div class="col-sm-9"><input type="text" placeholder="Anrede" name="standard_0" id="standard_0" maxlength="255" value="<?= $standard_0 ?>" class="form-control"></div>
+              </div>
+
+              <div class="form-group row">
+                <label class="col-form-label col-sm-3" for="standard_1">Vorname:</label>
+                <div class="col-sm-9"><input type="text" placeholder="Vorname" name="standard_1" id="standard_1" maxlength="255" value="<?= $standard_1 ?>" class="form-control"></div>
+              </div>
+
+              <div class="form-group row">
+                <label class="col-form-label col-sm-3" for="standard_2">Nachname:</label>
+                <div class="col-sm-9"><input type="text" placeholder="Nachname" name="standard_2" id="standard_2" maxlength="255" value="<?= $standard_2 ?>" class="form-control"></div>
+              </div>
+
+              <div class="form-group row">
+                <label class="col-form-label col-sm-3" for="standard_3">PLZ:</label>
+                <div class="col-sm-9"><input type="text" placeholder="PLZ" name="standard_3" id="standard_3" maxlength="255" value="<?= $standard_3 ?>" class="form-control"></div>
+              </div>
+
+              <div class="form-group row">
+                <label class="col-form-label col-sm-3" for="standard_4">Ort:</label>
+                <div class="col-sm-9"><input type="text" placeholder="Ort" name="standard_4" id="standard_4" maxlength="255" value="<?= $standard_4 ?>" class="form-control"></div>
+              </div>
+
+              <input type="hidden" name="doiSrc" value="anmeldeseite" />
+              <input type="hidden" name="doiSubscriptionPage" value="zipse" />
+              <div class="form-group row mt-2">
+                <div class="col-sm-9 offset-sm-3">
+                  <button class="btn btn-default btn-block" type="submit">Subscribe Contact</button>
+                </div>
+              </div>
+              <?php if (isset($response) && $response->isSuccess()) { ?>
+                <!-- redirect to ok.php -->
+                <script type="text/javascript">
+                  window.location.href = "ok.php";
+                </script>
+              <?php } elseif (isset($warning)) { ?>
+                <div class="alert alert-warning">
+                  <a class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                  <strong>Subscription failed</strong>
+                  <?= $warning['message'] ?>
+                </div>
+              <?php } elseif (isset($response)) { ?>
+                <div class="alert alert-danger">
+                  <a class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                  <strong>Subscription failed</strong>
+                </div>
+              <?php } ?>
+            </form>
+          </div>
+          <div class="bb-form-item">
+            <img class="form-image" src="img/voucher.png" alt="voucher">
+          </div>
         </div>
-      </div>
-    </section>
-    <section class="bg-gray-100 p-8">
-      <ul class="flex flex-col md:flex-row justify-center items-center py-10">
-        <li class="flex flex-col items-center"><img class="w-24 h-auto mb-4" src="img/process-1.png" alt="process-1">
-          <p class="text-center">Formular
-            ausfüllen</p>
-        </li>
-        <li class="p-8 h-full flex items-center grow">
-          <div class="w-12  transform rotate-90 md:rotate-0 border-4 border-black">
-          </div>
-        </li>
-        <li class="flex flex-col items-center opacity-50"><img class="w-24 h-auto mb-4" src="img/process-2.png" alt="process-2">
-          <p class="text-center">Bestätigungs-Mail
-            erhalten</p>
-        </li>
-        <li class="p-8 h-full flex items-center grow opacity-50">
-          <div class="w-12 transform rotate-90 md:rotate-0  border-4 border-black">
-          </div>
-        </li>
-        <li class="flex flex-col items-center opacity-50"><img class="w-24 h-auto mb-4" src="img/process-3.png" alt="process-3">
-          <p class="text-center">Klick auf
-            Link</p>
-        </li>
-        <li class="p-8 h-full flex items-center grow opacity-50">
-          <div class="w-12  transform rotate-90 md:rotate-0 border-4 border-black">
-          </div>
-        </li>
-        <li class="flex flex-col items-center opacity-50"><img class="w-24 h-auto mb-4" src="img/process-4.png" alt="process-4">
-          <p class="text-center">Anmeldung
-            erfolgreich</p>
-        </li>
-      </ul>
-    </section>
-    <section class="px-8 py-16">
-      <p class="text-sm">Ja, ich möchte den ZIPSE Newsletter abonnieren und bin über 16 Jahre alt. Ich erhalte den
-        Newsletter
-        kostenlos etwa alle
-        zwei Wochen mit allgemeinen Informationen, Prospekte und Produktnews, sowie Veranstaltungen und
-        spezielle Informationen.
-        Abmelden kann ich mich jederzeit über den Abmeldelink im Newsletter.
-        <br><br>
-        *Pflichtfeld. Wir verwenden die von Ihnen angegebenen Daten ausschließlich zum Versand des ZIPSE
-        Newsletters. Wir geben
-        Ihre Daten nicht an Dritte weiter.
-        Bitte beachten Sie auch unsere Datenschutzerklärung.
-      </p>
-    </section>
-  </main>
-  <footer class="bg-gray-100 px-8 py-8 flex flex-col md:flex-row items-center md:justify-between">
-    <ul class="flex flex-row items-center text-base mb-6 md:mb-0">
-      <li class="mr-4">Impressum</li>
-      <li class="mr-4">Datenschutz</li>
-      <li>AGB</li>
-    </ul>
-    <ul class="flex flex-row items-center">
-      <li> <img class="h-8 mr-4" src="img/social-facebook.png" alt="social facebook"> </li>
-      <li> <img class="h-8 mr-4" src="img/social-linkedin.png" alt="social linkedin"> </li>
-      <li> <img class="h-8 mr-4" src="img/social-instagram.png" alt="social instagram"> </li>
-      <li> <img class="h-8" src="img/social-youtube.png" alt="social youtube"> </li>
-    </ul>
-  </footer> -->
+      </section>
+
+
+
+
+      <section>
+        <ul class="process-list">
+          <li class="process-item"><img class="process-image" src="img/process-1.png" alt="process-1">
+            <p class="process-text">Formular
+              ausfüllen</p>
+          </li>
+          <li class="process-item">
+            <div class="process-divider">
+            </div>
+          </li>
+          <li class="process-item opacity-50"><img class="process-image" src="img/process-2.png" alt="process-2">
+            <p class="process-text">Bestätigungs-Mail
+              erhalten</p>
+          </li>
+          <li class="process-item opacity-50">
+            <div class="process-divider">
+            </div>
+          </li>
+          <li class="process-item opacity-50"><img class="process-image" src="img/process-3.png" alt="process-3">
+            <p class="process-text">Klick auf
+              Link</p>
+          </li>
+          <li class="process-item opacity-50">
+            <div class="process-divider">
+            </div>
+          </li>
+          <li class="process-item opacity-50"><img class="process-image" src="img/process-4.png" alt="process-4">
+            <p class="process-text">Anmeldung
+              erfolgreich</p>
+          </li>
+        </ul>
+      </section>
+
+      <section class="small-print-container">
+        <p class="small-print-text">Ja, ich möchte den ZIPSE Newsletter abonnieren und bin über 16 Jahre alt. Ich erhalte den
+          Newsletter
+          kostenlos etwa alle
+          zwei Wochen mit allgemeinen Informationen, Prospekte und Produktnews, sowie Veranstaltungen und
+          spezielle Informationen.
+          Abmelden kann ich mich jederzeit über den Abmeldelink im Newsletter.
+          <br><br>
+          *Pflichtfeld. Wir verwenden die von Ihnen angegebenen Daten ausschließlich zum Versand des ZIPSE
+          Newsletters. Wir geben
+          Ihre Daten nicht an Dritte weiter.
+          Bitte beachten Sie auch unsere Datenschutzerklärung.
+        </p>
+      </section>
+    </main>
+    <?php include('./footer.php'); ?>
   </div>
 </body>
 
